c1509ad4f6dfc7e190dec2d6ed78db81e3b8b09b
pelmegov <pelmegovsa@gmail.com>
2018-01-27 11:03:56 +0300

    added auditing in tire model

--- src/main/java/ru/pelmegov/euroshini/domain/Tire.java
+++ src/main/java/ru/pelmegov/euroshini/domain/Tire.java
@@ -20,7 +20,7 @@ import ru.pelmegov.euroshini.domain.enumeration.Technology;
 @Entity
 @Table(name = "tire")
 @Document(indexName = "tire")
-public class Tire implements Serializable {
+public class Tire extends AbstractAuditingEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
